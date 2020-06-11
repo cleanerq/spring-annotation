acaa2f53a3eacda8605ce6e0dab6598aafbe5f1d
cleanerq <qbyun130@163.com>
2020-06-11 14:49:54 +0800

    import

--- src/main/java/com/qby/aop/LogAspects.java
+++ src/main/java/com/qby/aop/LogAspects.java
@@ -1,6 +1,11 @@
 package com.qby.aop;
 
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
